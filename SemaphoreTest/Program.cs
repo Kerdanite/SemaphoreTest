8a31a998dec5b34f15f1fbdc3734e2de8f90046a
Joffrey Truyen <jtruyen@cirilgroup.com>
2021-08-17 18:46:31 +0200

    Add project files.

---
+++ SemaphoreTest/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using WaterSemaphore;
+
+namespace SemaphoreTest
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Water water = new Water();
+            Console.WriteLine("Please enter sequence of Hydrogen and Oxygen");
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "e")
+                {
+                    break;
+                }
+
+                await water.BuildWaterAsync(input);
+            }
+            
+
+        }
+    }
+}
---
+++ WaterSemaphore/Water.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WaterSemaphore
+{
+    public class Water
+    {
+        private Semaphore semaphoreO = new Semaphore(0, 1);
+        private Semaphore semaphoreH = new Semaphore(2, 2);
+        private int hCount = 0;
+
+        public async Task BuildWaterAsync(string input)
+        {
+            hCount = 0;
+            List<Task> tasks = new List<Task>();
+
+            foreach (var c in input)
+            {
+                switch (c)
+                {
+                    case 'O':
+                    {
+                        tasks.Add(OThread(ReleaseOxygen));
+                        break;
+                    }
+                    case 'H':
+                    {
+                        tasks.Add(HTread(ReleaseHydrogen));
+                        break;
+                    }
+                    default:
+                        break;
+                }
+            }
+
+            await Task.WhenAll(tasks);
+        }
+
+        private void ReleaseHydrogen()
+        {
+            Console.WriteLine("H");
+        }
+
+        private void ReleaseOxygen()
+        {
+            Console.WriteLine("O");
+        }
+
+        private async Task HTread(Action releaseH)
+        {
+            await Task.Delay(1);
+            semaphoreH.WaitOne();
+            releaseH();
+            hCount++;
+
+            if (hCount % 2 == 0)
+            {
+                semaphoreO.Release();
+            }
+        }
+
+        private async Task OThread(Action releaseO)
+        {
+            await Task.Delay(1);
+
+            semaphoreO.WaitOne();
+
+            releaseO();
+
+            semaphoreH.Release(2);
+        }
+    }
+}
